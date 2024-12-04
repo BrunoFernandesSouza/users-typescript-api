781db2badf9c41a5c829a590a5f91c0bc50a4067
BrunoFernandesSouza <brunators@gmail.com>
2024-12-03 21:54:17 -0300

    feat: ensure GetUsersMongoRepositoriy gets users from mongo

--- src/repositories/get-users/mongo-get-users.ts
+++ src/repositories/get-users/mongo-get-users.ts
@@ -1,15 +1,16 @@
 import { IGetUsersRepository } from "../../controllers/get-users/protocols";
+import { MongoClient } from "../../database/mongo";
 import { User } from "../../models/users";
 
 export class MongoGetUsersRepository implements IGetUsersRepository {
   async getUsers(): Promise<User[]> {
-    return [
-      {
-        firstName: "Bruno",
-        lastName: "Souza",
-        email: "bruno@gmail.com",
-        password: "123",
-      },
-    ];
+    const users = await MongoClient.db
+      .collection<Omit<User, "id">>("users")
+      .find({})
+      .toArray();
+    return users.map(({ _id, ...rest }) => ({
+      ...rest,
+      id: _id.toHexString(),
+    }));
   }
 }
